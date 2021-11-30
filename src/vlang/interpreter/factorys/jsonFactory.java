714b63f4dfe2f5a77e945b1a68c5cb8457f9df33
Versocial <whx_albert@bupt.edu.cn>
2021-11-30 16:58:01 +0800

    暂存

--- src/vlang/interpreter/factorys/jsonFactory.java
+++ src/vlang/interpreter/factorys/jsonFactory.java
@@ -1,5 +1,6 @@
 package vlang.interpreter.factorys;
 
+import netscape.javascript.JSObject;
 import vlang.globalSetting;
 import vlang.interpreter.globalInfo;
 import vlang.interpreter.interpreter;
@@ -7,21 +8,31 @@ import vlang.interpreter.interpreterFactory;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
 public class jsonFactory implements interpreterFactory {
 
     @Override
     public interpreter createBy(String path) {
+        Scanner scanner=null;
         try {
-            Scanner scanner=new Scanner(new File(path));
-        } catch (FileNotFoundException e) {
-            System.out.println("[]FileNotFound:");
+            scanner=new Scanner(Paths.get(path), StandardCharsets.UTF_8.name());
+        } catch (IOException e) {
             e.printStackTrace();
         }
         globalSetting.log.info("starting interpreter by "+path+"..");
         interpreter newInterpreter=new interpreter(new globalInfo());
 
+        String str="";
+        while (scanner.hasNextLine())
+            str+=scanner.nextLine();
 
+
+        JSONObject
 
         return newInterpreter;
     }
