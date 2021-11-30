4a37c0c764c0f09d463324aad2e6cf8152893b77
Versocial <whx_albert@bupt.edu.cn>
2021-11-30 09:28:22 +0800

    输入暂时就这样吧

--- src/vlang/io/easyIo/easyInputMedia.java
+++ src/vlang/io/easyIo/easyInputMedia.java
@@ -2,23 +2,18 @@ package vlang.io.easyIo;
 
 import vlang.io.media.inputMedia;
 
-import javax.swing.plaf.metal.DefaultMetalTheme;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.stream.Stream;
 
 public class easyInputMedia implements inputMedia<easyRowInput> {
     private String ans = "";
     private Semaphore timeOut=new Semaphore(0);
     private long latestTime=0;
     private Lock lockLatestTime=new ReentrantLock();
-    private Scanner scanner=new Scanner(System.in);
     private BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(System.in));
 
     @Override
@@ -28,50 +23,19 @@ public class easyInputMedia implements inputMedia<easyRowInput> {
         Thread readIn= new Thread(()->{
             try {
                 while (true) {
-                    boolean hasNext=false;
-                    while(!hasNext) {
-                        //System.out.println("before av");
-                        int n= 0;
-                        try {
-                            n = System.in.available();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        //System.out.println("hasNext: "+n);
-
-                        hasNext=  n >0;
-                        if(hasNext)
-                            break;
-                        //System.out.println("sleep");
-                        Thread.sleep(1000);
-                    }
-                    //System.out.println("read");
-                    //String just=(scanner.nextLine());
-                   // ans+=just;
-                    //System.out.println("ans:" + ans+" just:"+just);
+                    String append=bufferedReader.readLine();
+                    if(Thread.interrupted())
+                        break;
                     lockLatestTime.lock();
+                    ans+=append;
                     latestTime = System.currentTimeMillis();
                     lockLatestTime.unlock();
                 }
-            } catch (InterruptedException e) {
-                boolean hasNext=false;
-                //System.out.println("finishing");
-                do {
-                    Stream<String>ss= bufferedReader.lines();//.forEach((s)->{ans+=s;});
-                    // ans += .;
-                    System.out.println(ss);
-                    System.out.println( ss.count());
-
-                    ss.forEach((s -> {
-                        System.out.println(s);
-                        ans+=s;}));
-                    System.out.println("cool");
-                }
-                while (hasNext);
-                System.out.println("finished");
+            }catch (IOException e){
+                System.out.println("io stopped.");
                 return;
             }
-
+            System.out.println("io interrupted.");
         });
         new Thread(new clock(silenceTime * 1000L)).start();
 
@@ -82,14 +46,14 @@ public class easyInputMedia implements inputMedia<easyRowInput> {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         readIn.interrupt();
-        try {
-            readIn.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println("ans="+ans);
-        return new easyRowInput(ans);
+        String ansInput;
+        lockLatestTime.lock();
+            System.out.println("ans="+ans);
+            ansInput=ans;
+        lockLatestTime.unlock();
+        return new easyRowInput(ansInput);
     }
 
 
